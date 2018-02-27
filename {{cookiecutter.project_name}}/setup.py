078894feffdf4415fb23c9a0e2fd22d98c530846
ktpr1223 <michael.pero1223@gmail.com>
2018-02-28 01:10:35 +0900

    setup.pyの雛形を書いた

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+name = '{{coockiecutter.project_name}}'
+
+setup(
+    name=name,
+    version='0.1',
+    author='ktpr1223',
+    packages=['src', 'src.preprocess'])
