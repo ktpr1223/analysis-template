d70a813d932bf019249985238d8a917a52cba19f
ktpr1223 <michael.pero1223@gmail.com>
2018-02-28 02:14:29 +0900

    configにファイルを追加し、サンプルをconfigを使うものに変更した

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -6,4 +6,5 @@ setup(
     name=name,
     version='0.1',
     author='ktpr1223',
+    # packagesは指定する必要あり
     packages=['src', 'src.preprocess'])
--- {{cookiecutter.project_name}}/src/preprocess/prep.py
+++ {{cookiecutter.project_name}}/src/preprocess/prep.py
@@ -1,3 +1,9 @@
+import os
+import yaml
+
+
 def prep():
-    print('Prep')
+    path = os.path.dirname(os.path.abspath(__file__))
+    print(
+        yaml.load(open(os.path.join(path.split('src')[0], 'config/prep.yml'))))
     return 2
